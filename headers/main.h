feef7099c408625fde4f8d68619bec966bc2299b
electronOS <39605620+electronOS@users.noreply.github.com>
2018-06-10 10:34:18 -0400

    CRITICAL SECURITY PATCH
    electron 0.1 patch notes:
    Fixes CRITICAL security bug!

--- headers/main.h
+++ headers/main.h
@@ -12,8 +12,8 @@ extern void WTP(unsigned short port, unsigned char data); // write port
 extern void IDT_LOAD(unsigned long *iptr); // load idt
 extern void handle_module(char command[1024]);
 int cmd_count = 0; // command write location
-char HELP_STR[1024] = "help"; // help string
-char KERN_STR[1024] = "kernel"; // kernel string (for kernel info)
+const char HELP_STR[1024] = "help"; // help string
+const char KERN_STR[1024] = "kernel"; // kernel string (for kernel info)
 
 struct IDT_entry {
 	unsigned short int lower_bits;
