0d68619f487e22adf14b6aa52db56fef2fc396bb
electronOS <39605620+electronOS@users.noreply.github.com>
2018-06-12 10:52:30 -0400

    0.1.6 - URGENT
    0.1.6 fixes an URGENT kernel vulnerability!

--- headers/module.h
+++ headers/module.h
@@ -2,6 +2,6 @@ typedef (*inptr)(int); // for modules
 inptr modules[131072]; // modules
 
 int is_mod_count; // count
-int is_mod[131072]; // for modules
+int *is_mod = (int*) 0xfffb38; // make is_mod unusable for a buffer overflow
 void add_module(int port, inptr ptr); // add module
 void run_module(int port, int i); // run module
