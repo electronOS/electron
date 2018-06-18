e903fae373d5a29fb383252910d37f543f57ad92
electronOS <39605620+electronOS@users.noreply.github.com>
2018-06-18 18:25:21 -0400

    Update boot.asm

--- source/boot.asm
+++ source/boot.asm
@@ -1,3 +1,8 @@
+; electronOS by iH8Ra1n.
+; NO WARRANTY OR LICENSE IN ANY WAY.
+; created in 2018.
+; ASSUME ALL FILES WERE CREATED BY IH8RA1N UNLESS STATED OTHERWISE! 
+
 bits 32
 section .text
 	align 4
